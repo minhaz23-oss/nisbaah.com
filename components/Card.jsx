0d2848fb822b8a67b5c28db5676e20c1de2ae057
Minhaz Fardin <minhazfardin25@gmail.com>
2024-06-15 16:27:34 +0600

    fixed issues

--- components/Card.jsx
+++ components/Card.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Card = ({img,text,num}) => {
   return (
     <div className='w-[250px] h-[320px] border-2 border-violet rounded-[10px] shadow-2xl flex flex-col justify-center items-center text-center'>
-       <Image src={`/${img}`} width={200} height={100} />
+       <Image src={`/${img}`} width={200} height={100} alt='pic'/>
     <p className=' mt-2 text-[18px] '> {text} </p>
        <h1 className=' text-[40px]'>{num}</h1>
     </div>
